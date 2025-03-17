356f9afb2fe2c7e803fff784d6700f4c7b7afc5c
sharan-0222 <sharanmenonc@gmail.com>
2025-03-17 11:06:08 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,24 @@
+num=int(input("please enter a number- "))
+if num>10 :
+   if num<20 :
+      print("number is between 10 and 20")
+   else :
+     print("number is not between 10 and 20")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
